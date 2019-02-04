181eebdb9c4e9191b8fb59487fd5d261c904a65d
Jor_Poon <jormpoon@gmail.com>
2019-02-04 17:27:27 -0500

    added clickevent to console.log hi for log in button

--- Components/home-page.js
+++ Components/home-page.js
@@ -1,5 +1,18 @@
-class LogInBtn {
-    constructor(btn) {
-        this.btn = btn;
-    }
-}
+// class LogInBtn {
+//     constructor(btn) {
+//         this.btn = btn;
+
+//         btn.addEventListener("click", () => this.loggin());
+//     }
+
+//     loggin() {
+//         console.log("hi");
+//     }
+// }
+
+let headBtn = document.querySelector(".navBtn");
+
+headBtn.addEventListener("click", () => {
+    console.log("hi");
+})
+
