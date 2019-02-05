e5c68bd7c20201d73985b1dfd44ab58aa02be2cc
Jor_Poon <jormpoon@gmail.com>
2019-02-04 19:11:05 -0500

    created nodelist of cards, but cannot get click event to work yet

--- Components/home-page.js
+++ Components/home-page.js
@@ -10,9 +10,26 @@
 //     }
 // }
 
+class CardContent {
+    constructor(card) {
+        this.card = card;
+        card.addEventListener("click", () => { this.describe()})
+    }
+
+    describe() {
+       //this.card.classList.toggle("group list");
+       console.log("DISPLAY PLEASE")
+    }
+}
+
+let cards = document.querySelectorAll(".card")
+cards.forEach(card => {new CardContent(card)});
+console.log(cards);
 let headBtn = document.querySelector(".navBtn");
 
 headBtn.addEventListener("click", () => {
     console.log("hi");
 })
+
+
 
